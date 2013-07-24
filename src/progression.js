7298668d12ffbbfe6cf0372d4a5d507b5ced69b3
Kieran Klaassen <kieranklaassen@gmail.com>
2013-07-24 11:58:18 -0500

    Refactored and commented code

--- src/progression.js
+++ src/progression.js
@@ -6,131 +6,145 @@
  */
 ;(function ( $, window, document, undefined ) {
 
-    var pluginName = "progression",
-        defaults = {
-            tooltipWidth: '200',
-            tooltipPosition: 'right',
-            tooltipOffset: '50',
-            showProgressBar: true,
-            showHelper: true,
-            validator: false,
-            tooltipFontSize: '14',
-            tooltipFontColor: 'ffffff',
-            progressBarBackground: 'ffffff',
-            progressBarColor: '6EA5E1',
-            tooltipBackgroundColor: 'a2cbfa',
-            tooltipPadding:'10',
-            tooltipAnimate:true
-        };
+  // Setting defaults
+  var pluginName = "progression", defaults = {
+    tooltipWidth: '200',
+    tooltipPosition: 'right',
+    tooltipOffset: '50',
+    showProgressBar: true,
+    showHelper: true,
+    validator: false,
+    tooltipFontSize: '14',
+    tooltipFontColor: 'ffffff',
+    progressBarBackground: 'ffffff',
+    progressBarColor: '6EA5E1',
+    tooltipBackgroundColor: 'a2cbfa',
+    tooltipPadding:'10',
+    tooltipAnimate:true
+  };
 
-       // $('<style>body { background-color: red; color: white; }</style>').appendTo('head');
+  function Plugin( element, options ) {
+    this.element = element;
+    this.currentForm = $(this.element);
+    this.options = $.extend( {}, defaults, options );
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
 
-    function Plugin( element, options ) {
-        this.element = element;
-        this.$elem = $(this.element);
-        this.options = $.extend( {}, defaults, options );
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
+  Plugin.prototype = {
 
-    Plugin.prototype = {
+    // Constructor function
+    init: function() {
 
-        init: function() {
+      // Setting helper function
+      function GetPercentage(a, b) {
+        return ((b / a) * 100);
+      }
 
-             data_prog = this.$elem.css('position','relative').find('[data-progression]');   
+      // Set form to relative position
+      // This is needed to position the progression tooltips correctly
+      progressionElements = this.currentForm.css('position','relative');
 
-            
+      // Find all progression elements in the form
+      progressionElements = this.currentForm.find('[data-progression]');
 
+      // Get first helper text to display on init
+      firstHelper =  progressionElements.first().attr('data-helper');
 
+      // If there is no helper text set it to empty string
+      if (firstHelper === undefined) {
+        firstHelper = '';
+      }
 
-          
+      // Style helper for different arrow positions
+      var arrowPosition = (this.options.tooltipPosition == 'right') ? 'border-color: transparent #'+this.options.tooltipBackgroundColor+' transparent transparent;' : 'border-color: transparent transparent transparent #'+this.options.tooltipBackgroundColor+'';
 
-            items =  data_prog.length;
-            thisid = this.$elem.attr('id');
-            firstoffset =  data_prog.first().position().top;
-            firsthelper =  data_prog.first().attr('data-helper');
-          
-                function GetPercentage(a, b) {
-                    return ((b / a) * 100);
-                }
+      // Build the HTML for the tooltip
+      html = $('\
+        <div  class="syco_tooltip"\
+              data-tooltip="'+this.currentForm.attr('id')+'"\
+              style=" padding:'+this.options.tooltipPadding+'px;\
+                      top:'+progressionElements.first().position().top+'px;\
+                      position:absolute;\
+                      background:#'+this.options.tooltipBackgroundColor+';\
+                      '+this.options.tooltipPosition+':-'+(parseInt(this.options.tooltipWidth) + parseInt(this.options.tooltipOffset))+'px;\
+                      width:'+this.options.tooltipWidth+'px"\
+        >\
+          <span class="triangle_'+this.options.tooltipPosition+'" style="'+arrowPosition+'"></span>\
+          <p style="font-size:'+this.options.tooltipFontSize+'px;color:#'+this.options.tooltipFontColor+'">\
+            <span class="tooltip_helper">\
+              <span data-index="1" >1</span> /'+progressionElements.length+'\
+            </span>\
+            '+firstHelper+'\
+          </p>\
+          <div class="percentagebar" style="background:#'+this.options.progressBarBackground+'"">\
+            <div class="percentagebarinner" style="background:#'+this.options.progressBarColor+'"></div>\
+            <span class="percent">0%</span>\
+          </div>\
+        </div>');
 
-            if (data_prog.first().attr('data-helper') !== undefined) {
-                  firsthelper =  data_prog.first().attr('data-helper');
-            } else {
-                firsthelper = '';
-            }
+      if(this.options.tooltipAnimate){
+        html.addClass('animated');
+        html.find('.percentagebarinner').addClass('animated');
+      }
 
-            if (this.options.showProgressBar === false) { 
-                $display = 'display:none;';
-            } else {
-                  $display = '';
-            }
+      // Hide percentage bar when it is disabled
+      if(!this.options.showProgressBar) {
+        html.find('.percentagebar').hide();
+      }
 
-            if (this.options.showHelper === false) {
-              $display2 = 'display:none;';
-            } else {
-                  $display2 = '';
-            }
+      // Hide tooltip helper text when it is disabled
+      if(!this.options.showHelper) {
+        html.find('.tooltip_helper').hide();
+      }
 
-             var animate = (this.options.tooltipAnimate) ? '-webkit-transition: top .3s ease-in-out;-moz-transition: top .3s ease-in-out;-o-transition: top .3s ease-in-out;transition: top .3s ease-in-out;' : '';
-             var animate2 = (this.options.tooltipAnimate) ? '-webkit-transition: width .3s ease-in-out;-moz-transition: width .3s ease-in-out;-o-transition: width .3s ease-in-out;transition: width .3s ease-in-out;' : '';
-             var arrowPosition = (this.options.tooltipPosition == 'right') ? 'border-color: transparent #'+this.options.tooltipBackgroundColor+' transparent transparent;' : 'border-color: transparent transparent transparent #'+this.options.tooltipBackgroundColor+'';
+      // Add the tooltip in the form
+      this.currentForm.prepend(html);
 
+      // Get all progression elements
+      progressionElements.each(function(){
 
-            marginRight = parseInt(this.options.tooltipWidth) + parseInt(this.options.tooltipOffset);
-            myhtml = $('<div class="syco_tooltip" data-tooltip="'+thisid+'" style="'+animate+'padding:'+this.options.tooltipPadding+'px;top:'+firstoffset+'px;position:absolute;background:#'+this.options.tooltipBackgroundColor+';'+this.options.tooltipPosition+':-'+marginRight+'px;width:'+this.options.tooltipWidth+'px" ><span class="triangle_'+this.options.tooltipPosition+'" style="'+arrowPosition+'"></span><p style="'+$display2+'font-size:'+this.options.tooltipFontSize+'px;color:#'+this.options.tooltipFontColor+'"><span class="tooltip_helper"><span data-index="1" >1</span>/'+items+'</span> '+firsthelper+'</p><div class="percentagebar" style="'+$display+'background:#'+this.options.progressBarBackground+'""><div class="percentagebarinner" style="'+animate2+'background:#'+this.options.progressBarColor+'"></div><span class="percent" '+$display+'>0%</span></div></div>');
-            this.$elem.prepend(myhtml);
-            thiswidth = this.$elem.find('.syco_tooltip').width();
+        var currentElement = $(this);
 
-             data_prog.each(function(){
+        // Bind to action on a field to move the tooltip
+        currentElement.bind('live focus change',function(){
 
-                var $this1 = $(this);
-                var offset = $this1.position().top;
+          thisprogressionlength = currentElement.parent().parent().find('[data-progression]').length;
+          alldataprogression = currentElement.parent().parent().find('[data-progression]');
 
-                $this1.bind('live focus change',function(){
+          thisId2=currentElement.parent().parent().attr('id');
+          thistooltip = $('[data-tooltip="'+thisId2+'"]');
+          thishelper= currentElement.attr('data-helper');
 
-                   thisprogressionlength = $this1.parent().parent().find('[data-progression]').length;
-                   alldataprogression = $this1.parent().parent().find('[data-progression]');
+          // Get current helper text to display on init
+          thishelper =  currentElement.attr('data-helper');
 
-                    thisid2=$this1.parent().parent().attr('id');
-                    thistooltip = $('[data-tooltip="'+thisid2+'"]');
-                    thishelper= $this1.attr('data-helper');
+          // If there is no helper text set it to empty string
+          if (thishelper === undefined) {
+            thishelper = '';
+          }
 
-                    if ($this1.attr('data-helper') !== undefined) {
-                            thishelper= $this1.attr('data-helper');
-                    } else {
-                        thishelper = '';
-                    }
+          index = parseInt($('#'+thisId2).find('[data-progression]').index(currentElement)) + 1;
+          percentage = GetPercentage(thisprogressionlength, index).toFixed(0);
+          thistooltip.find('p').html('<span class="tooltip_helper"><span data-index="1" >'+index+'</span>/'+thisprogressionlength+'</span> '+thishelper).parent().find('.percentagebarinner').css( "width",parseInt(percentage)+'%').next().html(parseInt(percentage)+'%');
+          thistooltip.css( "top", currentElement.position().top+'px' );
 
-                    index = parseInt($('#'+thisid2).find('[data-progression]').index($this1)) + 1;
-                    percentage = GetPercentage(thisprogressionlength, index).toFixed(0);
-                    thistooltip.find('p').html('<span class="tooltip_helper"><span data-index="1" >'+index+'</span>/'+thisprogressionlength+'</span> '+thishelper).parent().find('.percentagebarinner').css( "width",parseInt(percentage)+'%').next().html(parseInt(percentage)+'%');
-                    thistooltip.css( "top", offset+'px' );
-
-                });
-
-            });
-            
-         
-            
-        },
-
-        yourOtherFunction: function(el, options) {
-            // some logic
-        }
-    };
-
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
-            }
         });
-    };
+
+      });
+
+    }
+  };
+
+  // A really lightweight plugin wrapper around the constructor,
+  // preventing against multiple instantiations
+  $.fn[pluginName] = function ( options ) {
+    return this.each(function () {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" + pluginName, new Plugin( this, options ));
+      }
+    });
+  };
 
 })( jQuery, window, document );
-
-
